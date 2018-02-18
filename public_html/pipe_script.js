bd744a5c43f845c2a024bc2c0a3f62aa643e97c9
Vlad <novlad26@gmail.com>
2018-02-18 12:30:36 +0300

    Wrote full worked version, but working with numbers is not a good idea.

--- public_html/pipe_script.js
+++ public_html/pipe_script.js
@@ -5,12 +5,12 @@
  */
 function do_division(){
 
-
+	//ввод, преобразование
     var first_number = +(document.getElementById('first_number').value);
     var second_number = +(document.getElementById('second_number').value);
-    var first_number_binary = parseInt(first_number.toString(2), 10);
+    var first_number_binary = parseInt(first_number.toString(2), 2); //переделать?
 	//first_number_binary = parseInt(first_number_binary, 10);
-    var second_number_binary = parseInt(second_number.toString(2), 10);
+    var second_number_binary = parseInt(second_number.toString(2), 2); //переделать?
 	//second_number_binary = parseInt(second_number_binary, 10);
     var second_number_binary_addit = ~second_number_binary + 1;
  //document.getElementById('result').innerHTML = first_number_binary;  
@@ -23,38 +23,61 @@ function do_division(){
 	
     //document.write(number_of_digits_in_first + "   ");
     //document.write(number_of_digits_in_second + "   ");
-    document.write(residue_of_first + "   ");
+    //document.write(residue_of_first + "   ");
     
-    for(number_of_digits_in_first = 0; residue_of_first > 0; number_of_digits_in_first++){
+	
+    for(number_of_digits_in_first = 0b0; residue_of_first > 0b0; number_of_digits_in_first++){
         residue_of_first = residue_of_first >> 1;
     }
     var number_of_digits_in_second;
     var residue_of_second = second_number_binary;
-    for(number_of_digits_in_second = 0; residue_of_second > 0; number_of_digits_in_second++){
-        residue_of_second = residue_of_second << 1;
+    for(number_of_digits_in_second = 0b0; residue_of_second > 0b0; number_of_digits_in_second++){
+        residue_of_second = residue_of_second >> 1;
     }
     
 
-    var number_of_shifts = number_of_digits_in_first - number_of_digits_in_second;
-    if(number_of_digits_in_first > number_of_digits_in_second){
+    //делаем все необходимые предварительные сдвиги
+    if(first_number_binary > second_number_binary){
+		var number_of_shifts = number_of_digits_in_first - number_of_digits_in_second;
         second_number_binary = second_number_binary << number_of_shifts;
-        var bigest_number = first_number_binary;
+		second_number_binary_addit = second_number_binary_addit << number_of_shifts;
     }
-    else if(number_of_digits_in_first < number_of_digits_in_second)
+    else if(first_number_binary < second_number_binary){
         document.getElementById('result').innerHTML = 0;        
-    second_number_binary_addit = second_number_binary_addit << number_of_shifts;
+		return;
+	}
 	
-	
-    var result_number = 0;
-    var result_number_binary = result_number.toString(2);
+	//деление
+    var result_number_binary = 0b0;
+	var partial_balance = first_number_binary;
+	//var partial_balance = first_number_binary.toString(2);
+	//second_number_binary_addit = second_number_binary_addit.toString(2);
     for(var iteration = 0; iteration < number_of_shifts + 1; iteration++){
-		var partial_balance = first_number_binary + second_number_binary_addit;
+		partial_balance = partial_balance + second_number_binary_addit; //перепиать сложение!!!!!!!!!!!!!!!!
 		result_number_binary = result_number_binary << 1;
-		if(partial_balance > bigest_number){
+		var partial_balance = partial_balance.toString(2);
+		if(partial_balance[0] == "-"){
+			partial_balance = parseInt(partial_balance, 2);
+			partial_balance = partial_balance + second_number_binary; //восстановление остатка
 			
 		}
+		/*if(partial_balance.length - number_of_digits_in_first == 1 || partial_balance.length == number_of_digits_in_first){
+			partial_balance = parseInt(partial_balance, 2);
+			partial_balance = partial_balance + second_number_binary; //восстановление остатка
+		}*/
+		//else if(partial_balance.length - number_of_digits_in_first == 2 && partial_balance.substr(0,2) == "10"){ //если появляется "10", то преобразуем их в "0" (удаляем)
+		else if(partial_balance.length - number_of_digits_in_first == 2 && partial_balance.substr(0,2) == "10"){	
+			partial_balance = partial_balance.substring(2);
+			result_number_binary = result_number_binary + 0b1;
+			partial_balance = parseInt(partial_balance, 2);
+		}
+		else if(partial_balance.length <= number_of_digits_in_first ){
+			result_number_binary = result_number_binary + 0b1;
+			partial_balance = parseInt(partial_balance, 2);
+		}
+		partial_balance = partial_balance << 1; //строка не сдвигается!!!!!!!!!!!!!!!!!!
     }
-
+	document.getElementById('result').innerHTML = result_number_binary;
 }
 
 document.getElementById('say').addEventListener('click', do_division);
